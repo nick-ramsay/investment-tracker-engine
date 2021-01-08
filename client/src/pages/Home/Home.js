2c86509dd4b67155b5fed34f0f1fb6b4c3c7946c
Nick Ramsay <NickRamsay1989@gmail.com>
2021-01-08 23:03:34 +1100

    Added clock to application as basis for setting up scheduling

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -10,6 +10,19 @@ const override = "display: block; margin: 0 auto; border-color: #2F4F4F;";
 
 const Home = () => {
 
+    var [currentMoment, setCurrentMoment] = useState(new Date());
+    var [loading, setLoading] = useState(false);
+
+    let startClock = () => {
+        setInterval(clock, 1000);
+    }
+
+    const clock = () => {
+        let currentMomentVar = moment();
+        console.log(currentMoment);
+        setCurrentMoment(currentMoment => currentMomentVar);
+    }
+
     const scrapeAdvancedStats = () => {
         console.log("Called scrapeAdvancedStats...");
         API.scrapeAdvancedStats().then(res => {
@@ -17,16 +30,26 @@ const Home = () => {
         });
     }
 
-    var [loading, setLoading] = useState(false);
-
     useEffect(() => {
-        console.log("useEffect Called...")
+        console.log("useEffect Called...");
+        startClock();
     }, []) //<-- Empty array makes useEffect run only once...
 
     return (
         <div>
             <NavbarLoggedOut />
             <div className="container">
+                <div className="row">
+                    <div className="col-md-12 text-center">
+                        <h2>{moment(currentMoment).format("dddd, D MMMM YYYY")}</h2>
+                    </div>
+                    <div className="col-md-12 text-center">
+                        <h2>{moment(currentMoment).format("hh:mm A")}</h2>
+                    </div>
+                </div>
+                <div className="row">
+
+                </div>
                 <div className="col-md-12 d-flex justify-content-center text-center mt-2">
                     <BarLoader
                         css={override}
@@ -34,18 +57,20 @@ const Home = () => {
                         color={"#D4AF37"}
                         loading={loading}
                     />
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th>Worker</th>
-                                <th></th>
-                            </tr>
-                            <tr>
-                                <td>Scrape Yahoo! Advanced Stats</td>
-                                <td><button className="btn btn-sm btn-dark" onClick={() => scrapeAdvancedStats()}>Run</button></td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div className="row">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <th>Worker</th>
+                                    <th></th>
+                                </tr>
+                                <tr>
+                                    <td>Scrape Yahoo! Advanced Stats</td>
+                                    <td><button className="btn btn-sm btn-dark" onClick={() => scrapeAdvancedStats()}>Run</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
