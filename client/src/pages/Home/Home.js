11030bc0c76e101e6e1faf87ff57ce0a1d61f1b9
Nick Ramsay <NickRamsay1989@gmail.com>
2021-01-08 20:43:16 +1100

    Can now run scraper

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -10,6 +10,12 @@ const override = "display: block; margin: 0 auto; border-color: #2F4F4F;";
 
 const Home = () => {
 
+    const scrapeAdvancedStats = () => {
+        console.log("Called scrapeAdvancedStats...");
+        API.scrapeAdvancedStats().then(res => {
+            console.log(res);
+        });
+    }
 
     var [loading, setLoading] = useState(false);
 
@@ -21,14 +27,25 @@ const Home = () => {
         <div>
             <NavbarLoggedOut />
             <div className="container">
-                <div className="col-md-12 mt-2">
+                <div className="col-md-12 d-flex justify-content-center text-center mt-2">
                     <BarLoader
                         css={override}
                         height={10}
                         color={"#D4AF37"}
                         loading={loading}
                     />
-
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Worker</th>
+                                <th></th>
+                            </tr>
+                            <tr>
+                                <td>Scrape Yahoo! Advanced Stats</td>
+                                <td><button className="btn btn-sm btn-dark" onClick={() => scrapeAdvancedStats()}>Run</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
